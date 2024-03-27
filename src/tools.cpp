7ff77f1d8217b968423b9cbc5525117f48c9a925
yuliamorozova <yuliyamorozova0@mail.com>
2024-03-27 14:46:02 +0100

    src/tools.cpp getChannelName(): ch_prefix changed, see comment in the func

--- src/tools.cpp
+++ src/tools.cpp
@@ -90,7 +90,7 @@ void Server::welcomeClient(Client &c)
  */
 int getChannelName(std::string &cname)
 {
-	std::string ch_prefix = "&#";
+	std::string ch_prefix = "&#!+";
 
 	if (cname.length() > 50)
 		return -1;
